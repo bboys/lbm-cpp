9ad6cce6c77a32370e27407d0e62e6339c332b3f
Mark Redeman <markredeman@gmail.com>
2015-10-24 16:46:11 +0200

    Restructure the main file and simplify it somewhat
    Preferably the main file should do almost nothing and delegate our input to a LBM_Command that would parse the input (argv, argc) using a similar as Laravel's command specification.

--- src/main.cpp
+++ src/main.cpp
@@ -2,93 +2,40 @@
 #include <ctime>
 #include <fstream>
 #include <sstream>
-using namespace Domains;
+#include <iostream>
 
+#include "Domains/DomainInitializer.h"
+#include "Domains/BoxedDomain.h"
+#include "Domains/LidDrivenCavity.h"
+#include "Domains/PointDomain.h"
+#include "VelocitySets/d2q9.h"
+
+#include "LBM/node.h"
 #include "LBM/parallel.h"
 
+using namespace Domains;
+
 // Global variables
-size_t dx;
-size_t dy;
+size_t dx = 80;
+size_t dy = 80;
 size_t ITERATIONS;
-size_t REPORT_PER_ITERATION;
+size_t REPORT_PER_ITERATION = 10;
 size_t P;
 
-size_t askForIterations(int argc, char** argv)
+int main(int argc, char **argv)
+try
 {
-    size_t iterations = 1000;
-    if (argc > 2) {
-        std::istringstream iss(argv[2]);
-        if ((iss >> iterations) )
-            return iterations;
-    }
+    ITERATIONS = askForIterations(argc, argv);
+    P          = askForProcessors(argc, argv);
 
-    return iterations;
+    bsp_init(simulate, argc, argv);
+    simulate();
+
+    return 0;
 }
-
-size_t askForProcessors(int argc, char** argv)
+catch(int x)
 {
-    size_t P;
-    if (argc > 1)
-    {
-        std::istringstream iss(argv[1]);
-        if ((iss >> P) )
-            return P;
-    }
-    else
-    {
-        std::cout << "How many processors do you want to use?" << std::endl;
-        std::cin >> P;
-    }
-
-    if (P > bsp_nprocs())
-    {
-        std::cout << "Sorry, not enough processors available." << std::endl;
-        exit(0);
-    }
-    return P;
-}
-
-std::string createFileName(size_t iteration, std::string setName, std::string domainName, std::vector<size_t> domainSize)
-{
-    // (iter, "D2Q9", "Lid_Driven_Cavity_5000_", domainSize);
-    // logs/D2Q9_Lid_Driven_Cavity_5000_50_100
-    // logs/D2Q9_Boxed_10x10_100.txt
-    std::stringstream ss;
-    ss << "logs/" << setName << "_" << domainName << "_";
-    for (size_t idx = 0; idx < domainSize.size() - 1; ++idx)
-        ss << domainSize[idx] << "x";
-    ss << domainSize[domainSize.size() - 1];
-    ss << "_" << ITERATIONS << "_" << iteration << ".txt";
-    return ss.str();
-}
-
-
-void createMatlabReport(LBM::Simulation &sim, size_t iter, std::vector<size_t> domainSize)
-{
-    // Create a output file
-    std::ofstream out(createFileName(iter, "D2Q9", "PERIODIC", domainSize), std::ios::out | std::ios::app);
-    Reporting::MatlabReporter reporter(out);
-    sim.report(reporter);
-}
-
-void showVector(std::vector<size_t> vector, std::ofstream &out)
-{
-    for (size_t dim = 0; dim < (vector.size() - 1); ++dim)
-        out << vector[dim] << ", ";
-    out << vector[vector.size() - 1] << ")";
-}
-
-void logSimulationData(std::vector<size_t> domainSize)
-{
-    std::ofstream out("logs/timings.log", std::ios::out | std::ios::app);
-    size_t p = bsp_nprocs();
-    // Start by writing basic info to the file
-    // out << "LBM simulation using " << p <<
-    //     " processors to perform " << ITERATIONS << " iterations on the 'dummy' domain " <<
-    //     ", with set: " << "D2Q9" << " and domain size: (";
-    out << "LBM, p: " << p << ", it: " << ITERATIONS << ", ds (";
-    showVector(domainSize, out);
-    out << ", ";
+    return x;
 }
 
 void simulate()
@@ -107,13 +54,7 @@ void simulate()
     D2Q9 set;
     auto domainSize = {dx, dy};
 
-    // All domains except DomainInitializer are currently not working!
-        // LidDrivenCavityDomain initializer(&set, domainSize);
-        // PointDomain initializer(&set, domainSize);
-        // BoxedDomain initializer(&set, domainSize, s, p);
     DomainInitializer initializer(&set, domainSize, s, p);
-
-    // Create simulation
     LBM::Simulation sim(&initializer);
 
     // Log initialization time and prepare computation time
@@ -149,17 +90,79 @@ void simulate()
     bsp_end();
 }
 
-int main(int argc, char **argv)
+size_t askForIterations(int argc, char** argv)
 {
-    ITERATIONS = askForIterations(argc, argv);
-    REPORT_PER_ITERATION = 10;
-    dx = 80;
-    dy = 80;
+    size_t iterations = 11;
+    if (argc > 2) {
+        std::istringstream iss(argv[2]);
+        if ((iss >> iterations) )
+            return iterations;
+    }
 
-    bsp_init(simulate, argc, argv);
+    return iterations;
+}
 
-    P = askForProcessors(argc, argv);
-    simulate();
+size_t askForProcessors(int argc, char** argv)
+{
+    size_t P;
+    if (argc > 1)
+    {
+        std::istringstream iss(argv[1]);
+        if ((iss >> P) )
+            return P;
+    }
+    else
+    {
+        std::cout << "How many processors do you want to use?" << std::endl;
+        std::cin >> P;
+    }
 
-    return 0;
+    if (P > bsp_nprocs())
+    {
+        std::cout << "Sorry, not enough processors available." << std::endl;
+        exit(0);
+    }
+    return P;
+}
+
+void logSimulationData(std::vector<size_t> domainSize)
+{
+    std::ofstream out("logs/timings.log", std::ios::out | std::ios::app);
+    size_t p = bsp_nprocs();
+    // Start by writing basic info to the file
+    // out << "LBM simulation using " << p <<
+    //     " processors to perform " << ITERATIONS << " iterations on the 'dummy' domain " <<
+    //     ", with set: " << "D2Q9" << " and domain size: (";
+    out << "LBM, p: " << p << ", it: " << ITERATIONS << ", ds (";
+    showVector(domainSize, out);
+    out << ", ";
+}
+
+void showVector(std::vector<size_t> vector, std::ofstream &out)
+{
+    for (size_t dim = 0; dim < (vector.size() - 1); ++dim)
+        out << vector[dim] << ", ";
+    out << vector[vector.size() - 1] << ")";
+}
+
+void createMatlabReport(LBM::Simulation &sim, size_t iter, std::vector<size_t> domainSize)
+{
+    // Create a output file
+    std::ofstream out(createFileName(iter, "D2Q9", "PERIODIC", domainSize), std::ios::out | std::ios::app);
+    Reporting::MatlabReporter reporter(out);
+    sim.report(reporter);
+}
+
+std::string createFileName(size_t iteration, std::string setName, std::string domainName, std::vector<size_t> domainSize)
+{
+    // (iter, "D2Q9", "Lid_Driven_Cavity_5000_", domainSize);
+    // logs/D2Q9_Lid_Driven_Cavity_5000_50_100
+    // logs/D2Q9_Boxed_10x10_100.txt
+    std::stringstream ss;
+    ss << "logs/" << setName << "_" << domainName << "_";
+    for (size_t idx = 0; idx < domainSize.size() - 1; ++idx)
+        ss << domainSize[idx] << "x";
+    ss << domainSize[domainSize.size() - 1];
+    ss << "_" << ITERATIONS << "_" << iteration << ".txt";
+    return ss.str();
 }
--- src/main.h
+++ src/main.h
@@ -1,36 +1,25 @@
 #ifndef INCLUDED_MAIN
 #define INCLUDED_MAIN
-/*
-// #define PARALLEL (TODO)
-#ifndef PARALLEL
-    struct Messenger
-    {
-        double *source;
-        size_t p;
-        double *dest;
-    };
-    // als dest naar een distriubion wijst, dan hebben we hier geen nodes nodig
-    communicate(Messenger *messengers, size_t totalMessengers);
-#endif
-*/
-#include <iostream>
+
 #include <string>
 #include <vector>
 
-#include "VelocitySets/VelocitySet.h"
-#include "LBM/node.h"
-#include "VelocitySets/d2q9.h"
-#include "Reporting/MatlabReporter.h"
-
-#include "Domains/DomainInitializer.h"
-#include "Domains/BoxedDomain.h"
-#include "Domains/LidDrivenCavity.h"
-#include "Domains/PointDomain.h"
-
 #include "LBM/Simulation.h"
 
+int main(int argc, char **argv);
+void simulate();
+
+// Setup functions
+size_t askForIterations(int argc, char** argv);
+size_t askForProcessors(int argc, char** argv);
+
+// Logging functions
+void logSimulationData(std::vector<size_t> domainSize);
+void showVector(std::vector<size_t> vector, std::ofstream &out);
+
+// Reporting functions
+void createMatlabReport(LBM::Simulation &sim, size_t iter, std::vector<size_t> domainSize);
 std::string createFileName(size_t iteration, std::string setName, std::string domainName, std::vector<size_t> domainSize);
 
-int main(int argc, char **argv);
 
 #endif
